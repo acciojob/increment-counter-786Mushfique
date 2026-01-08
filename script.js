226a8fc03efc70c45828661da8548d3eec8a4a18
Md Mushfique Raza <114329904+786Mushfique@users.noreply.github.com>
2026-01-08 13:31:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let parentDiv = document.getElementById("count")
+let display = parentDiv.querySelector("p")
+let count = 0
+function Increment(n){
+  count++;
+  display.innerText = count ;
+  }
+}
